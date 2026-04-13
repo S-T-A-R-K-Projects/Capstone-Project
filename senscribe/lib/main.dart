1fb2fb3d2b87589bd324448c244c5c93f98a21ce
reewaz-appdata <reewaz.rijal03@gmail.com>
2026-04-12 23:25:54 -0500

    change to logic
    made some changes to the logic so its more fluent and doesnt over lap with the main home page

--- senscribe/lib/main.dart
+++ senscribe/lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:adaptive_platform_ui/adaptive_platform_ui.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'theme/app_theme.dart';
 import 'navigation/main_navigation.dart';
+import 'screens/introduction_page.dart';
 
 void main() {
   GoogleFonts.config.allowRuntimeFetching = true;
@@ -48,11 +50,29 @@ class SenScribeApp extends StatefulWidget {
 
 class _SenScribeAppState extends State<SenScribeApp> {
   final ThemeProvider _themeProvider = ThemeProvider();
+  bool _introCompleted = false;
 
   @override
   void initState() {
     super.initState();
     _themeProvider.addListener(_onThemeChanged);
+    _checkIntroCompleted();
+  }
+
+  Future<void> _checkIntroCompleted() async {
+    final prefs = await SharedPreferences.getInstance();
+    final completed = prefs.getBool('intro_completed') ?? false;
+    setState(() {
+      _introCompleted = completed;
+    });
+  }
+
+  Future<void> _markIntroCompleted() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('intro_completed', true);
+    setState(() {
+      _introCompleted = true;
+    });
   }
 
   @override
@@ -74,7 +94,9 @@ class _SenScribeAppState extends State<SenScribeApp> {
       cupertinoLightTheme: AppTheme.cupertinoLightTheme,
       cupertinoDarkTheme: AppTheme.cupertinoDarkTheme,
       themeMode: _themeProvider.themeMode,
-      home: const MainNavigationPage(),
+      home: _introCompleted 
+        ? const MainNavigationPage()
+        : IntroductionPage(onDone: _markIntroCompleted),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
